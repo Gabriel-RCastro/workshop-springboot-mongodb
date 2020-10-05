6203cbfbf05610c25011aa8fb0f9808a55c07c95
Gabriel Rodrigues de Castro <gabrielrodriguesdecastro@gmail.com>
2020-10-05 13:47:33 -0300

    Acrescentando comentários aos posts

--- src/main/java/br/com/gubee/workshopspringbootmongodb/domain/Post.java
+++ src/main/java/br/com/gubee/workshopspringbootmongodb/domain/Post.java
@@ -1,11 +1,14 @@
 package br.com.gubee.workshopspringbootmongodb.domain;
 
 import br.com.gubee.workshopspringbootmongodb.dto.AuthorDTO;
+import br.com.gubee.workshopspringbootmongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,7 +20,9 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
+
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -68,6 +73,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
