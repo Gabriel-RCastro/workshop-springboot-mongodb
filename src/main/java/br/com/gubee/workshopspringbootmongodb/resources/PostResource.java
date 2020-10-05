4185d8ec320cd935a4f8c6dc5345d940da22c240
Gabriel Rodrigues de Castro <gabrielrodriguesdecastro@gmail.com>
2020-10-05 13:41:28 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/gubee/workshopspringbootmongodb/resources/PostResource.java
@@ -0,0 +1,21 @@
+package br.com.gubee.workshopspringbootmongodb.resources;
+
+import br.com.gubee.workshopspringbootmongodb.domain.Post;
+import br.com.gubee.workshopspringbootmongodb.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
