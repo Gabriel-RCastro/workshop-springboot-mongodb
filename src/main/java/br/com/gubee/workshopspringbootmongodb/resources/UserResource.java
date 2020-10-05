0375c8aec51b8eedbf27cec714f99d36152d4350
Gabriel Rodrigues de Castro <gabrielrodriguesdecastro@gmail.com>
2020-10-05 11:16:37 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/gubee/workshopspringbootmongodb/resources/UserResource.java
+++ src/main/java/br/com/gubee/workshopspringbootmongodb/resources/UserResource.java
@@ -6,8 +6,10 @@ import br.com.gubee.workshopspringbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,4 +27,11 @@ public class UserResource {
         List<UserDTO> listDTO = list.stream().map(UserDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
 }
