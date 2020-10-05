ff97f36ddffeffcea8e1c6e8b2c6b010db514c97
Gabriel Rodrigues de Castro <gabrielrodriguesdecastro@gmail.com>
2020-10-05 11:32:45 -0300

    Inserção de usuário com POST

--- src/main/java/br/com/gubee/workshopspringbootmongodb/services/UserService.java
+++ src/main/java/br/com/gubee/workshopspringbootmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.gubee.workshopspringbootmongodb.services;
 
 import br.com.gubee.workshopspringbootmongodb.domain.User;
+import br.com.gubee.workshopspringbootmongodb.dto.UserDTO;
 import br.com.gubee.workshopspringbootmongodb.repository.UserRepository;
 import br.com.gubee.workshopspringbootmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
