f13fab45c0d2c60ee0fbe81ad0380a87ef00e209
Gabriel Rodrigues de Castro <gabrielrodriguesdecastro@gmail.com>
2020-10-05 14:04:16 -0300

    Consulta simples com query methods

--- src/main/java/br/com/gubee/workshopspringbootmongodb/resources/PostResource.java
+++ src/main/java/br/com/gubee/workshopspringbootmongodb/resources/PostResource.java
@@ -1,10 +1,13 @@
 package br.com.gubee.workshopspringbootmongodb.resources;
 
 import br.com.gubee.workshopspringbootmongodb.domain.Post;
+import br.com.gubee.workshopspringbootmongodb.resources.util.URL;
 import br.com.gubee.workshopspringbootmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
