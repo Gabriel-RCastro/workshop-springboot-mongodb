518db55e543e31ed208f0fc3c3b6a35e06a3e041
Gabriel Rodrigues de Castro <gabrielrodriguesdecastro@gmail.com>
2020-10-05 10:02:38 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/br/com/gubee/workshopspringbootmongodb/WorkshopSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gubee.workshopspringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
